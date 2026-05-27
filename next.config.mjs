24f2bd3af8452458f586f5342e611e0a531d7614
tianle0602-commits <tianle0602@gmail.com>
2026-05-27 20:15:41 +0800

    Configure Next.js for static export and GitHub Pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  //开启静态导出(GitHub Pages必须)  
   output: "export",
+  //适配你的仓库名/fajian
+  basePath: "/fajian",
+  assetPrefix: "/fajian/",
+  //解决图片路径问题
+  trailingSlash:true,
+  images:{
+    unoptimized:true,
+  }
 };
 
 export default nextConfig;
